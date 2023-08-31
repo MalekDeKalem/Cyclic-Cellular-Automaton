f58277f97c4d7e5c43fa079e18e25910d41693e5
MalekDeKalem <dimitri200169@gmail.com>
2023-08-31 11:38:56 +0200

    neighbor functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::cell;
+use std::{cell, time::Duration};
 
 use nannou::prelude::*;
 
@@ -17,11 +17,13 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
+#[derive(Default)]
 struct Cell {
     _state: u8,
     _pos: Point2,
     _size: u8,
 }
+
 
 
 struct Model {
@@ -36,10 +38,11 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
+    app.set_loop_mode(LoopMode::rate_fps(1.0));
 
     // Init the cells randomly at first
     let mut _cells: Vec<Cell> = Vec::new();
-    let size: i32 = 10;
+    let size: i32 = 25;
 
     for j in (-HEIGHT / 2 + size/2..=HEIGHT / 2 - size/2).rev().step_by(size as usize) {
         for i in (-WIDTH/2+size as i32/2..WIDTH/2+size as i32/2).step_by(size as usize) {
@@ -57,47 +60,54 @@ fn modulo(a: i32, b: i32) -> i32 {
 }
 
 fn update(app: &App, _model: &mut Model, update: Update) {
-}
-
-fn neighbor(_model: &mut Model, index: i32) {
     let cell_size = _model._cells[0]._size as f32;
     let width = WIDTH / cell_size as i32;
     let height = HEIGHT / cell_size as i32;
+
+    let closure_vec: Vec<Box<dyn Fn(i32, i32) -> i32>> = vec![
+        Box::new(|i: i32, j: i32| j * width + modulo(i - 1, width)),
+        Box::new(|i: i32, j: i32| j * width + modulo(i + 1, width)),
+        Box::new(|i: i32, j: i32| modulo(j + 1, height) * width + i),
+        Box::new(|i: i32, j: i32| modulo(j - 1, height) * width + i),
+        Box::new(|i: i32, j: i32| modulo(j - 1, height) + modulo(i - 1, width)),
+        Box::new(|i: i32, j: i32| modulo(j - 1, height) + modulo(i + 1, width)),
+        Box::new(|i: i32, j: i32| modulo(j + 1, height) + modulo(i - 1, width)),
+        Box::new(|i: i32, j: i32| modulo(j + 1, height) + modulo(i + 1, width)),
+    ];
+
+
+    
+
     for j in 0..HEIGHT / cell_size as i32 {
         for i in 0..WIDTH / cell_size as i32 {
             let current_cell = &_model._cells[(j * width + i) as usize];
-            let mut left_cell = &mut _model._cells[(j * width + modulo(i - 1, width)) as usize];
-            let mut right_cell = &mut _model._cells[(j * width + modulo(i + 1, width)) as usize];
-            let mut bottom_cell = &mut _model._cells[(modulo(j + 1, height) * width + i) as usize];
-            let mut top_cell = &mut _model._cells[(modulo(j - 1, height) * width + i) as usize];
-            let mut top_left_cell = &mut _model._cells[(modulo(j - 1, height) + modulo(i - 1, width)) as usize];
-            let mut top_right_cell = &mut _model._cells[(modulo(j - 1, height) + modulo(i + 1, width)) as usize];
-            let mut bottom_left_cell = &mut _model._cells[(modulo(j + 1, height) + modulo(i - 1, width)) as usize];
-            let mut bottom_right_cell = &mut _model._cells[(modulo(j + 1, height) + modulo(i + 1, width)) as usize];
+            let current_state = current_cell._state; // Clone the state value
 
+            for k in 0..closure_vec.len() {
+                let index = closure_vec[k](i, j) as usize;
+
+                let target_cell = &mut _model._cells[index];
+                if (current_state + 1) > target_cell._state {
+                    target_cell._state = current_state;
+                }
+            }
         }
     }
 }
+
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
     let mut index = 0;
-    let width = WIDTH / 10 as i32;
-    let height = HEIGHT / 10 as i32;
+    let width = WIDTH / 25 as i32;
+    let height = HEIGHT / 25 as i32;
     let test_index = modulo(0 - 1, height) * width + modulo(0 - 1, width);
     for cell in &model._cells {
-        if index == test_index {
-            draw.rect()
-            .wh(vec2(cell._size as f32, cell._size as f32))
-            .x_y(cell._pos.x, cell._pos.y)
-            .color(Srgb::<u8>::new(0, 255, 0));
-        } else {
             draw.rect()
             .wh(vec2(cell._size as f32, cell._size as f32))
             .x_y(cell._pos.x, cell._pos.y)
             .color(Srgb::<u8>::new(cell._state, 0, 0));
-        }
         index += 1;
     }
 
